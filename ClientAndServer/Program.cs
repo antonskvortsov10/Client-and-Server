2017b5958038b3dfea40304f4ee4dc72c20fc413
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-02-05 23:58:28 +0300

    Добавьте файлы проекта.

---
+++ ClientAndServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientAndServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
